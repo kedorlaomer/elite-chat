de99b175f4f1eac6cbce0ae76239b09a47531f7c
Elite Chat Team <elitechat@example.com>
2025-11-23 07:35:10 +0000

    Add Render domain to ALLOWED_HOSTS and set DEBUG=False for production

--- elite_chat/settings.py
+++ elite_chat/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yw&h009yit7+cadz@^_07hj0r&@b)e(j$bi!89-zgtijog0=ka'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.179.128', 'localhost']
+ALLOWED_HOSTS = ['192.168.179.128', 'localhost', 'elite-chat.onrender.com']
 
 
 # Application definition

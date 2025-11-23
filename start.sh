848b36cf6ac979184413318bbffd41743fd202c1
Elite Chat Team <elitechat@example.com>
2025-11-23 08:47:42 +0000

    Activate virtual environment in start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@
 
 set -e  # Exit on any error
 
+# Activate virtual environment
+source venv/bin/activate
+
 # Check required environment variables
 if [ -z "$DJANGO_SUPERUSER_USERNAME" ] || [ -z "$DJANGO_SUPERUSER_EMAIL" ] || [ -z "$DJANGO_SUPERUSER_PASSWORD" ]; then
     echo "Error: Required environment variables not set:"
